7627d087bb96c6e0ac7617448bd24ddd91f6717a
StevenJack666 <823772544@qq.com>
2023-11-25 18:42:33 +0800

    content:tg消息推送

---
+++ deploy/process.sh
@@ -0,0 +1,13 @@
+# 0 3 * * * /bin/bash /home/py/package/install/crawl_comm.sh >> /home/py/cron/cron.log 2>&1 &
+
+
+source ~/.bashrc
+conda init bash
+source  activate  crawl
+cd /home/py/package/vcrawl
+nohup python vcrawl.py  tg --task_id=1 --action=get_message > /home/py/package/install/logs/py_tg.log 2>&1 &
+pid=`ps ax | grep vcrawl | grep -v grep | awk -e '{print $1}'`
+#echo $pid >> /home/py/package/install/logs/process.log
+data_time=`date '+%Y-%m-%d %H:%M:%S'`
+line='__'
+echo ${data_time}${line}${pid}  >> /home/py/package/install/logs/process.log
